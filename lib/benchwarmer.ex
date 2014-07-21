fdaa0f66d392d797f8e8db90ff3943c25de66a1d
Matthew Rothenberg <mrothenberg@gmail.com>
2014-07-21 00:05:12 -0400

    make duration an argument

--- lib/benchwarmer.ex
+++ lib/benchwarmer.ex
@@ -8,7 +8,7 @@ defmodule Benchwarmer do
   """
 
   # minimum duration for a benchmark is 1 second (1Mμs)
-  @min_duration 1_000_000
+  @default_duration 1_000_000
 
   alias Benchwarmer.Results
 
@@ -25,34 +25,43 @@ defmodule Benchwarmer do
     1.2 sec   524K iterations   2.33 μs/op
   """
   # args is a list of arguments, as a list
-  def benchmark(f, args \\ [])
-  def benchmark(f, args) when is_function(f), do: benchmark([f], args)
-  def benchmark(functions, args) when is_list(functions) do
-    Enum.each(functions, fn(f) ->
-      IO.inspect f
+  def benchmark(f, args \\ [], min_duration \\ @default_duration)
+  def benchmark(f, args, min_duration) when is_function(f) do
+    benchmark([f], args, min_duration)
+  end
+  def benchmark(f, args, min_duration) when is_list(f) and is_list(args) do
+    Enum.each(f, fn(fp) ->
+      IO.inspect fp
 
-      results = do_benchmark(f, args)
+      results = do_benchmark(fp, args, min_duration)
       IO.puts results
     end)
   end
 
-  defp do_benchmark(f, args, r \\ %Results{}) do
+  defp do_benchmark(f, args, min_duration, results \\ %Results{}) do
     # TODO: does this help with the load timer issues?
     :timer.start()
 
     cond do
       # if elapsed test time is greater than minimum, return no matter what
-      r.duration >= @min_duration -> r
+      results.duration >= min_duration -> results
 
       # first time run for a single iteration
-      r.n == 0 ->
+      results.n == 0 ->
         {:ok, _, optime} = run_n_times(f, args, 1)
-        do_benchmark( f, args, %{r | n: 1, prev_n: 1, duration: optime} )
+        do_benchmark( f, args, min_duration,
+                      %{results | n: 1, prev_n: 1, duration: optime}
+                    )
 
       # default case, run with double iterations of previous instance
       true ->
-        {:ok, iters, optime} = run_n_times(f, args, r.prev_n * 2)
-        do_benchmark( f, args, %{r | n: r.n + iters, prev_n: iters, duration: r.duration + optime} )
+        {:ok, iters, optime} = run_n_times(f, args, results.prev_n * 2)
+        do_benchmark( f, args, min_duration,
+                      %{results | n: results.n + iters,
+                                  prev_n: iters,
+                                  duration: results.duration + optime
+                      }
+                    )
     end
   end
 
